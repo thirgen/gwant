4ff31614aa2be56c6ef8b27f4fece70ef99b5b56
Joe <22317125+thirgen@users.noreply.github.com>
2018-04-13 10:50:16 +0100

    Better card scrolling

--- Gwant/Assets/Scripts/CardScrollRect.cs
+++ Gwant/Assets/Scripts/CardScrollRect.cs
@@ -13,6 +13,7 @@ public class CardScrollRect : MonoBehaviour, IPointerEnterHandler,
     public bool rightClick;
     int cardCount = 2;
     public Vector3 pos;
+    Rect screenRect = new Rect(50, 50, Screen.width - 100, Screen.height - 100);
 
 	// Use this for initialization
 	void Start () {
@@ -85,10 +86,61 @@ public class CardScrollRect : MonoBehaviour, IPointerEnterHandler,
         Transform cards = transform.GetChild(0);
         GameObject first = cards.GetChild(0).gameObject;
         GameObject last = cards.GetChild(cards.childCount - 1).gameObject;
+        //
+        if (left && !IsOnScreen(last))
+            return true;
+        else if (!left && !IsOnScreen(first))
+            return true;
+        /*
         if (left && last.GetComponent<RectTransform>().position.x > Screen.width - 100)
             return true;
         else if (!left && first.GetComponent<RectTransform>().position.x < 100)
             return true;
+        */
         return false;
+    }
+
+    private bool IsOnScreen(GameObject go)
+    {
+        //https://answers.unity.com/questions/918955/is-there-a-simple-way-to-check-if-a-ui-element-is.html
+
+        bool valid = true;
+        Vector3[] corners = new Vector3[4];
+        go.GetComponent<RectTransform>().GetWorldCorners(corners);
+        
+        foreach (Vector3 v in corners)
+        {
+            if (!screenRect.Contains(v))
+            {
+                valid = false;
+                break;
+            }
+        }
+        return valid;
+    }
+
+    private void Update()
+    {
+        //DRAW CARD SCROLL SAFE ZONE
+        /* 
+        Transform cards = transform.GetChild(0);
+        GameObject first = cards.GetChild(0).gameObject;
+        GameObject last = cards.GetChild(cards.childCount - 1).gameObject;
+
+
+        Vector3[] firstcorners = new Vector3[4];
+        first.GetComponent<RectTransform>().GetWorldCorners(firstcorners);
+        Vector3[] lastcorners = new Vector3[4];
+        last.GetComponent<RectTransform>().GetWorldCorners(lastcorners);
+        for (int i = 0; i < 4; i += 2)
+        {
+            Debug.DrawLine(firstcorners[i], firstcorners[i + 1], Color.red);
+            Debug.DrawLine(lastcorners[i], lastcorners[i + 1], Color.green);
+        }
+        Debug.DrawLine(new Vector3(screenRect.xMin, screenRect.yMin), new Vector3(screenRect.xMin, screenRect.yMax), Color.blue);
+        Debug.DrawLine(new Vector3(screenRect.xMax, screenRect.yMin), new Vector3(screenRect.xMax, screenRect.yMax), Color.blue);
+        Debug.DrawLine(new Vector3(screenRect.xMin, screenRect.yMin), new Vector3(screenRect.xMax, screenRect.yMin), Color.cyan);
+        Debug.DrawLine(new Vector3(screenRect.xMin, screenRect.yMax), new Vector3(screenRect.xMax, screenRect.yMax), Color.cyan);
+        */
     }
 }
